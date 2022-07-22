7700cc89861dd0da1e59431485e993988e6386a9
denis-nahuamel <nahuamel.denis@gmail.com>
2022-07-22 21:18:08 +0000

    add get product function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { getProducts } from './services/product-service';
 
 function App() {
+  useEffect(()=> {
+    getProducts().then(response => console.log("response", response))
+  },[])
   return (
     <div className="App">
       <h1>Hello world</h1>
