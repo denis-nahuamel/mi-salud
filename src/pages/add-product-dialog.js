62a054186c4febbe1db6b563d833bd0759ea19e8
denis-nahuamel <nahuamel.denis@gmail.com>
2022-07-22 22:19:46 +0000

    add laboratory select

--- src/pages/add-product-dialog.js
+++ src/pages/add-product-dialog.js
@@ -1,10 +1,12 @@
 /** @jsxImportSource @emotion/react */
-import {css} from "@emotion/react";
+import { css } from "@emotion/react";
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import Select from '@mui/material/Select';
+
+import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import { containerDialog, input } from "../styles/product-style";
@@ -18,24 +20,28 @@ const AddProductDialog = (props) => {
     const handleListItemClick = (value) => {
         onClose(value);
     };
+    const handleProduct = (event) => {
+        event.preventDefault();
+        console.log(event)
+    }
 
     return (
         <div css={containerDialog}>
             <Dialog onClose={handleClose} open={open} css={containerDialog}>
                 <DialogTitle>Agregar Producto</DialogTitle>
+
                 <input placeholder="nombre" css={input}></input>
-                <Select
-                    labelId="demo-select-small"
-                    id="demo-select-small"
-                    label="Laboratorio"
-                >
-                    <MenuItem value={"Genfar"}>Genfar</MenuItem>
-                    <MenuItem value={"GSK"}>GSK</MenuItem>
-                    <MenuItem value={"Hersil"}>Hersil</MenuItem>
-                    <MenuItem value={"FarmaIndustria"}>FarmaIndustria”.</MenuItem>
-                </Select>
+                <select name="select" css={input}>
+                    <option value="Genfar">Genfar</option>
+                    <option value="GSK">GSK</option>
+                    <option value="Hersil">Hersil</option>
+                    <option value="FarmaIndustria">FarmaIndustria</option>
+                </select>
+                
                 <input placeholder="cantidad" css={input}></input>
                 <input placeholder="precio" css={input}></input>
+                <button type="submit">Enviar</button>
+
             </Dialog>
         </div>
     );
